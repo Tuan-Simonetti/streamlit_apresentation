f94bf0e83d943637dec56c9e59ba82266b387031
Tuan Simonetti <tuan.gomes@picpay.com>
2021-08-20 15:01:29 -0300

    Corrige as rotas dos paths

--- checkLibrariesInstaled/check_libraries_instaled.py
+++ checkLibrariesInstaled/check_libraries_instaled.py
@@ -9,18 +9,12 @@ def execute_home_page():
 def libraries_instaled():
     for library in libraries:
         ### Check library
-        print(f"""\n\n\n
-                        ------------------------------------------------------------------------------------------------
-                        Verificando se a biblioteca {library} encontra-se instalada e faz Upgrade da versao mais recente
-                        ------------------------------------------------------------------------------------------------""")
+        print(f"""\n\n\n------------------------------------------------------------------------------------------------\nVerificando se a biblioteca {library} encontra-se instalada e faz Upgrade da versao mais recente\n------------------------------------------------------------------------------------------------""")
         if library == "pip":
             os.system(f"python install --upgrade pip")
         else:
             os.system(f"pip install {library} -U")
-        print(f"""\n\n\n
-                        ------------------------------------------------------------------------------------------------
-                        A biblioteca {library} encontra-se instalada e atualizada
-                        ------------------------------------------------------------------------------------------------""")
+        print(f"""\n\n\n------------------------------------------------------------------------------------------------\nA biblioteca {library} encontra-se instalada e atualizada\n------------------------------------------------------------------------------------------------""")
 
 
     ### Show list of library instaled
