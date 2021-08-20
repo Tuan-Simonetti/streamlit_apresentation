83e6902678232e4f6376f056e57cfbb3b5494760
Tuan Simonetti <tuan.gomes@picpay.com>
2021-08-20 16:00:45 -0300

    Corrige as rotas dos paths

--- checkLibrariesInstaled/check_libraries_instaled.py
+++ checkLibrariesInstaled/check_libraries_instaled.py
@@ -9,15 +9,20 @@ def execute_home_page():
 def libraries_instaled():
     for library in libraries:
         ### Check library
-        #print(f"""\n\n\n------------------------------------------------------------------------------------------------\nVerificando se a biblioteca {library} encontra-se instalada e faz Upgrade da versao mais recente\n------------------------------------------------------------------------------------------------""")
+        print(f"""\n\n\n
+                        ------------------------------------------------------------------------------------------------
+                        Verificando se a biblioteca {library} encontra-se instalada e faz Upgrade da versao mais recente
+                        ------------------------------------------------------------------------------------------------""")
         if library == "pip":
-            #os.system(f"python install --upgrade pip")
-            pass
+            os.system(f"python install --upgrade pip")
         else:
             os.system(f"pip install {library} -U")
-        #print(f"""\n\n\n------------------------------------------------------------------------------------------------\nA biblioteca {library} encontra-se instalada e atualizada\n------------------------------------------------------------------------------------------------""")
+        print(f"""\n\n\n
+                        ------------------------------------------------------------------------------------------------
+                        A biblioteca {library} encontra-se instalada e atualizada
+                        ------------------------------------------------------------------------------------------------""")
 
 
     ### Show list of library instaled
     check_lib = os.system("pip list")
-    #print(f"________________________________\n{check_lib}")
+    print(f"________________________________\n{check_lib}")
--- checkLibrariesInstaled/libraries_list.py
+++ checkLibrariesInstaled/libraries_list.py
@@ -3,5 +3,5 @@ libraries = [
     "streamlit",
     "watchdog",
     "Pillow",
-    #"pip"
+    "pip"
 ]
